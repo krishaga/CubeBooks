fca996a4efff8019cba4c0073481065872a07ed9
Abhishek1345 <jawanpuriaabhishek@gmail.com>
2021-04-25 13:26:06 +0530

    files

---
+++ src/books.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './books.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+class Books extends React.Component{
+    render(){
+        return(
+            <section>
+             <div className="in">
+            <input type="text" placeholder="search"/>
+            <FontAwesomeIcon className="ico" icon={faSearch} />
+
+            </div>
+            <button>
+               go
+            </button>
+            </section>
+        )
+    }
+}
+export default Books;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom';
 import Ham from './ham.jsx';
 import Logo from './logo.jsx';
 import Header from './header.jsx';
+import Books from './books.jsx';
 import './index.css';
 ReactDOM.render(
-  <div>
+  <div id="page">
+  <div id="home">
   <Ham></Ham>
   <Logo></Logo>
   <div className="wrapper"><Header></Header></div>
   <h1>welcome to cube books, the no 1 place to get all your books for free</h1>
+</div>
+<div id="books">
+<Books></Books>
+</div>
 </div>,
   document.getElementById('root')
 );
