52ee66d3b9376b23b60d46d889db65d7d2281c0a
Abhishek1345 <jawanpuriaabhishek@gmail.com>
2021-04-26 15:56:26 +0530

    files

--- src/ham.jsx
+++ src/ham.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './ham.css';
 class Ham extends React.Component{
+// constructor(props){
+//     super(props);
+    
+//     }
+
+toggle=()=>{
+    document.getElementsByClassName("ham")[0].classList.toggle("anime");
+}
     render(){
         return (
-            <div className="ham">
+            <div className="ham" onClick={this.toggle}>
                 <div className="bar"></div>
                 <div className="bar"></div>
                 <div className="bar"></div>
