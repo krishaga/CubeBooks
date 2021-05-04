d7e55b12046907d6b40f97cddbabf556586f7103
Abhishek1345 <jawanpuriaabhishek@gmail.com>
2021-05-04 16:07:12 +0530

    files

--- src/Subject.jsx
+++ src/Subject.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './Subject.css';
 
 class Subject extends React.Component{
-    constructor(props){
-    super(props);
-    }
+    // constructor(props){
+    // super(props);
+    // }
 render(){
     return(
         <select value="physics">
